6798f63b2c4509ea8ab0a590b0535611f99f633b
dwane richards <dwanerich@gmail.com>
2020-02-06 00:47:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,7 +39,10 @@ class Song
     genre.songs << self unless genre.songs.include? self
   end
 
-  def self.find_by_name(name)
+  def find_by_name(name)
     all.find(&:name)
   end
+
+  def find_or_create_by_name(name)
+    find_by_name ||
 end
