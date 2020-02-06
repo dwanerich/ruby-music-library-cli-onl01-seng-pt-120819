509b38369f32b7a5a3be704b07e5045b10339146
dwane richards <dwanerich@gmail.com>
2020-02-06 01:20:40 +0000

    Automatically backed up by Learn

--- lib/conerns/findable.rb
+++ lib/conerns/findable.rb
@@ -4,4 +4,8 @@ module Concerns::Findable
     all.find {|song|song.name == song_name}
   end
 
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
+  end
+
 end
