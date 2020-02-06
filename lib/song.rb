198f0dac3035560675214944f03f1ad8e1eae56e
dwane richards <dwanerich@gmail.com>
2020-02-06 00:44:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,10 +11,6 @@ class Song
     self.genre = genre if genre
   end
 
-  def self.find_by_name(name)
-    all.find(&:name)
-  end
-
   def self.all
     @@all
   end
@@ -41,5 +37,9 @@ class Song
   def genre=(genre)
     @genre = genre
     genre.songs << self unless genre.songs.include? self
+  end
+
+  def self.find_by_name(name)
+    all.find(&:name)
   end
 end
