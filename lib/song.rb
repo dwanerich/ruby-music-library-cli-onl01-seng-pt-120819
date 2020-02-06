cf69e9b35ccff65fda7ba437dadab371cc64d51e
dwane richards <dwanerich@gmail.com>
2020-02-06 01:02:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,49 +1,57 @@
-class Song
+class Song < Super
 
-  attr_accessor :name
   attr_reader :artist, :genre
 
-  @@all = []
-
-  def initialize(name, artist = nil, genre = nil)
+  def initialize(name, artist=nil, genre=nil)
     @name = name
-    self.artist = artist if artist
-    self.genre = genre if genre
-  end
-
-  def self.all
-    @@all
-  end
-
-  def self.destroy_all
-    all.clear
-  end
-
-  def save
-    self.class.all << self
+    self.artist = artist if artist != nil
+    self.genre = genre if genre != nil
   end
 
   def self.create(name)
-    song = Song.new(name)
-    song.save
-    song
+    instance = Song.new(name)
+    instance.save
+    instance
   end
 
   def artist=(artist)
     @artist = artist
-    artist.add_song(self)
+    self.artist.add_song(self)
   end
 
   def genre=(genre)
     @genre = genre
-    genre.songs << self unless genre.songs.include? self
+    self.genre.add_song(self)
   end
 
-  def find_by_name(name)
-    all.find(&:name)
+  def self.new_from_filename(file_name)
+    file_bits = file_name.gsub(/(\.mp3)/,'')
+    file_bits = file_bits.split(" - ")
+    artist = Artist.find_or_create_by_name(file_bits[0])
+    genre = Genre.find_or_create_by_name(file_bits[2])
+    song = Song.find_or_create_by_name(file_bits[1])
+    song.genre = genre
+    song.artist = artist
+    song
   end
 
-  def self.find_or_create_by_name(name)
-    self.find_by_name(name) || Song.new(name)
+  def self.create_from_filename(file_name)
+    file = Song.new_from_filename(file_name)
+    file.save
+    file
   end
+
+  def self.find_by_name(song_name)
+    self.all.find {|song|song.name == song_name}
+  end
+
+  def self.find_or_create_by_name(song_name)
+    song = self.find_by_name(song_name)
+    if song == nil
+      self.create(song_name)
+    else
+      song
+    end
+  end
+
 end
