5790cb3b44c20151230dbc1c9c13cd97c13aae02
dwane richards <dwanerich@gmail.com>
2020-02-06 01:29:08 +0000

    Automatically backed up by Learn

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -12,4 +12,12 @@ class MusicImporter
 		files = files.collect{|file|file.scan(/(?<=mp3s\/)(.*)/)}.flatten #disgusting RegEx that matches everything after the path
 	end
 
+  def import
+		new_files = self.files
+		new_files.each do |file|
+			new_song = Song.create_from_filename(file)
+		end
+
+	end
+
 end
